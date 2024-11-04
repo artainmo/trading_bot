cb2413f90826e59716d930863b9ba7b22aa03179
Arthur Tainmont <53705599+artainmo@users.noreply.github.com>
2024-11-04 11:36:29 +0100

    Update __init__.py

---
+++ trade_bot/code/utils/__init__.py
@@ -0,0 +1,2 @@
+#__init__.py is used to transform a directory into a package/library.
+#This file can be empty. The only time you want to actually put things in it is if you want to import only specific files. If you keep it empty you can import from everything. 
